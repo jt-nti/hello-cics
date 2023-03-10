ea927da233d4487ad5efab4f88077bdb70efa730
James Taylor <jamest@uk.ibm.com>
2023-03-10 10:58:57 +0000

    Update dev container
    Automatically unlock keyring
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

---
+++ .devcontainer/zowe-bashrc.sh
@@ -0,0 +1,8 @@
+
+# Unlock the Gnome keyring automatically. See:
+# https://docs.zowe.org/stable/user-guide/cli-configure-scs-on-headless-linux-os/#unlocking-the-keyring-automatically
+if test -z "$DBUS_SESSION_BUS_ADDRESS" ; then
+  exec dbus-run-session -- $SHELL
+fi
+
+printf '\n' | gnome-keyring-daemon -r --unlock --components=secrets
